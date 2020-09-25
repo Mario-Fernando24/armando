6358cafd826cab6690a4663d8d52cff428b8f89b
HG Max <hgmax@iMac-de-HG.local>
2020-09-25 12:38:37 -0500

    Fixed run gmaps in iOS

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,9 +8,8 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDUQnTL9u27wa_0kyYsg0N4shm9qMiCtvI")
+    GMSServices.provideAPIKey("AIzaSyCTRZ53lU31V1qm3R2UaSgOkTxs9w4dxFg")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
-
